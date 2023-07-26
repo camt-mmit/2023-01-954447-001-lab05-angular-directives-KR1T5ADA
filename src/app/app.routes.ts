079ebf56df1bd978450f9b6a1c82b984dc4c9d9c
Kritsada Intaprom <kritsada.itp@gmail.com>
2023-07-27 01:34:20 +0700

    lab05

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { TryOneComponent } from './try-one/try-one.component';
 import { TryTwoComponent } from './try-two/try-two.component';
 import { TryThreeComponent } from './try-three/try-three.component';
+import { DynamicSectionComponent } from './dynamic-section/dynamic-section.component';
 
 export const routes: Routes = [
   { path: 'try-one', component: TryOneComponent },
   { path: 'try-two', component: TryTwoComponent },
   { path: 'try-three', component: TryThreeComponent },
+  { path: 'lab05', component: DynamicSectionComponent },
 ];
